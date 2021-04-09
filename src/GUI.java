09df7ceaf2a0a85b2fb078f54fb9b660b55e120e
zdworkin <zachdworkin@lclark.edu>
2021-04-09 13:34:21 -0700

    Allow user to switch images with arrow keys

--- src/GUI.java
+++ src/GUI.java
@@ -4,9 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
@@ -15,57 +14,14 @@ import javax.swing.text.StyleConstants;
 public class GUI extends JFrame {
 
     private final JPanel buttonPanel;
-    private JButton inputRecipe = new JButton("Input Recipe");
-    private ImageComponent displayedImage;
+    private final JButton inputRecipe = new JButton("Input Recipe");
+    private JLabel displayedImage;
+    private ImageComponent imageComponent;
 
     public GUI () {
-        GridBagLayout layout = new GridBagLayout();
-        setLayout(layout);
-        SimpleAttributeSet as = new SimpleAttributeSet();
-        StyleConstants.setAlignment(as, StyleConstants.ALIGN_CENTER);
-        buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-
-        buttonPanel.add(inputRecipe);
-
-        InputMap imap = buttonPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-        imap.put(KeyStroke.getKeyStroke("RIGHT"), "panel.next");
-        imap.put(KeyStroke.getKeyStroke("LEFT"), "panel.prev");
-        AbstractAction nextFrameAction = new StepAction(1);
-        AbstractAction previousFrameAction = new StepAction(-1);
-        //map those names of inputs to actions
-        ActionMap actionMap = buttonPanel.getActionMap();
-        actionMap.put("panel.next", nextFrameAction);
-        actionMap.put("panel.prev", previousFrameAction);
-
-        inputRecipe.addActionListener(new InputRecipeAction());
-
-        displayedImage = new ImageComponent(new ImageIcon(getClass().getResource("cook_book.png")).getImage());
-        displayedImage.setBorder(BorderFactory.createEtchedBorder());
-
+        this.buttonPanel = new JPanel();
+        initAll();
         render();
-    }
-
-    private void render() {
-        pack();
-        validate();
-        repaint();
-    }
-
-    private class StepAction extends AbstractAction {
-        private int number;
-
-        public StepAction(int direction) {
-            this.number = direction;
-        }
-
-        public void actionPerformed(ActionEvent event) {
-        }
-    }
-
-    private class InputRecipeAction extends AbstractAction{
-        public void actionPerformed(ActionEvent event) {
-        }
     }
 
     public static void main(String[] args) {
@@ -83,5 +39,86 @@ public class GUI extends JFrame {
             gui.addWindowListener(exitListener);
             gui.setVisible(true);
         });
+    }
+
+    private void initAll() {
+        initializeLayout();
+        initializeButtonPanel();
+        initializeKeystrokeActions();
+        initImageComponent();
+        addActionListeners();
+    }
+
+    private void addActionListeners() {
+        inputRecipe.addActionListener(new InputRecipeAction());
+    }
+
+    private void initImageComponent() {
+        try {
+            imageComponent = new ImageComponent();
+        } catch (IOException ioe) {
+            JOptionPane.showMessageDialog(null,"Default Image not found.");
+        }
+
+        imageComponent.setBorder(BorderFactory.createEtchedBorder());
+        displayedImage = new JLabel();
+        add(displayedImage);
+    }
+
+    private void initializeKeystrokeActions() {
+        InputMap imap = buttonPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        imap.put(KeyStroke.getKeyStroke("RIGHT"), "panel.next");
+        imap.put(KeyStroke.getKeyStroke("LEFT"), "panel.prev");
+        AbstractAction nextFrameAction = new StepAction(1);
+        AbstractAction previousFrameAction = new StepAction(-1);
+
+        ActionMap actionMap = buttonPanel.getActionMap();
+        actionMap.put("panel.next", nextFrameAction);
+        actionMap.put("panel.prev", previousFrameAction);
+    }
+
+    private void initializeButtonPanel() {
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.add(inputRecipe);
+        add(buttonPanel);
+    }
+
+    private void initializeLayout() {
+        GridBagLayout layout = new GridBagLayout();
+        setLayout(layout);
+        SimpleAttributeSet as = new SimpleAttributeSet();
+        StyleConstants.setAlignment(as, StyleConstants.ALIGN_LEFT);
+    }
+
+    private void render() {
+        displayedImage.setIcon(new ImageIcon(imageComponent.getImage()));
+        pack();
+        revalidate();
+        repaint();
+    }
+
+    private class StepAction extends AbstractAction {
+        private int number;
+
+        public StepAction(int direction) {
+            this.number = direction;
+        }
+
+        public void actionPerformed(ActionEvent event) {
+            try {
+                imageComponent.setImage("cook_book1.png");
+            } catch (IOException | IllegalArgumentException e) {
+                JOptionPane.showMessageDialog(null, "Image not found");
+            }
+
+            render();
+        }
+    }
+
+    private class InputRecipeAction extends AbstractAction{
+        public InputRecipeAction() {}
+
+        public void actionPerformed(ActionEvent event) {
+        }
     }
 }
--- src/ImageComponent.java
+++ src/ImageComponent.java
@@ -1,16 +1,36 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
 class ImageComponent extends JComponent {
     private Image image;
+    private Path resourcesDirectory;
+    private File imageFile;
 
-    public ImageComponent(Image image) {
-        this.image = image;
+    public ImageComponent() throws IOException {
+        resourcesDirectory = Path.of((new File(System.getProperty("user.dir"))).getAbsolutePath()).resolve("resources");
+        this.image = setImageFile("cook_book.png");
     }
 
-    public void paintComponent(Graphics graphics) {
+    public Image getImage() { return this.image;}
+
+    public void setImage(String image) throws IOException {
+        this.image = setImageFile(image);
+    }
+
+    public Image setImageFile(String image) throws IOException {
+        imageFile = resourcesDirectory.resolve(image).toFile();
+        return ImageIO.read(imageFile);
+    }
+
+    public void paintComponent(Graphics g) {
         if (image == null) return;
 
-        graphics.drawImage(image, 0, 0, null);
+        g.drawImage(image, 0, 0, null);
+        Graphics2D g2 = (Graphics2D) g;
     }
 }
