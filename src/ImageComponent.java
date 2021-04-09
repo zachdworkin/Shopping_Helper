41db2297a357a2c451890d891146fb26577f7899
zdworkin <zachdworkin@lclark.edu>
2021-04-09 11:15:35 -0700

    Update Image Component

--- src/ImageComponent.java
+++ src/ImageComponent.java
@@ -1,2 +1,16 @@
-public class ImageComponent {
+import javax.swing.*;
+import java.awt.*;
+
+class ImageComponent extends JComponent {
+    private Image image;
+
+    public ImageComponent(Image image) {
+        this.image = image;
+    }
+
+    public void paintComponent(Graphics graphics) {
+        if (image == null) return;
+
+        graphics.drawImage(image, 0, 0, null);
+    }
 }
